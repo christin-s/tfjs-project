a2b1816864604f7660452155fc849988626ddc01
christin-s <cy.wulandari@gmail.com>
2023-02-25 14:12:43 +0000

    Call loadGraphModel API and preprocess an image into a 4-D tensor

---
+++ run-tfjs-model.js
@@ -0,0 +1,48 @@
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs');
+
+const modelUrl =
+  'https://tfhub.dev/tensorflow/tfjs-model/ssdlite_mobilenet_v2/1/default/1';
+
+let model;
+
+//load COCO-SSD graph model from TensorFlow Hub
+//this model expects a 4-D tensor with image pixel values
+const loadModel = async function () {
+  console.log(`loading model from ${modelUrl}`);
+
+  model = await tf.loadGraphModel(modelUrl, {fromTFHub: true});
+
+  return model;
+}
+
+// preprocessing image before passing it to the graph model
+// convert image to the appropriate Tensor
+const processInput = function (imagePath) {
+  console.log(`preprocessing image ${imagePath}`);
+
+  const image = fs.readFileSync(imagePath);
+  const buf = Buffer.from(image);
+  const uint8array = new Uint8Array(buf);
+
+  // convert an image into a 3-d tensor using the decodeImage() API
+  // increase dimension to 4-d using expandDims()
+  return tf.node.decodeImage(uint8array, 3).expandDims();
+}
+
+// run
+// loadModel().then(model => {
+//   console.log(model);
+// })
+if (process.argv.length < 3) {
+  console.log('please pass an image to process. ex:');
+  console.log('  node run-tfjs-model.js /path/to/image.jpg');
+} else {
+  // e.g., /path/to/image.jpg
+  let imagePath = process.argv[2];
+
+  loadModel().then(model => {
+    const inputTensor = processInput(imagePath);
+    inputTensor.print();
+  })
+}
